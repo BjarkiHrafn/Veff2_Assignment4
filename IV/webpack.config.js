57c06788363f3c858013bdf7a015029e17219ac1
bjarki15 <bjarki15@ru.is>
2018-02-27 12:39:59 +0000

    first push - bjarki

---
+++ IV/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+            { test: /\.less$/, use: [
+                { loader: 'style-loader' },
+                { loader: 'css-loader' },
+                { loader: 'less-loader' }
+            ]}
+        ]
+    },
+    watch: true,
+    resolve: {
+        extensions: ['.js', '.jsx', '.less']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+        open: true
+    },
+    devtool: 'inline-source-map',
+    plugins: [new HtmlWebpackPlugin({
+        title: 'ChatRoomJS',
+        template: './index.html'
+    })]
+};
